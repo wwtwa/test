aac59227b199e7628ec7bd5384c4c7a1678134cc
wwtwa <139637471+wwtwa@users.noreply.github.com>
2024-10-22 03:48:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,5 +89,6 @@ def extract_number(row):
 
 # 全ての行の"大分類"の"number"を抽出してリストを作成
 numbers = df['properties'].apply(extract_number).tolist()
+groups = df.groupby(numbers[2])
 
-st.write(numbers)
+st.write(groups)
